726ce1648a1bb7beb17a2cfd5573298618f593d8
Somnath Chakraborty <61010000+somnath352@users.noreply.github.com>
2022-04-19 00:58:51 +0530

    Update find_repeat_missing_element.cpp

--- find_repeat_missing_element.cpp
+++ find_repeat_missing_element.cpp
@@ -1,128 +1,104 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// 1st approach (using count sort)
-
-vector<int> find_missing_repeating(vector<int>& array) {
-    int n = array.size() + 1;
-    vector<int> subs_array(n, 0); // Initialize the substitude array 0 size N+1  
-    vector<int> ans; // Initialize answer array
-    
-    for(int i = 0;i<array.size();i++) {
-        subs_array[array[i]]++;
-    }
-    
-    for(int i = 1;i<=array.size();i++) {
-        if(subs_array[i] == 0 || subs_array[i] > 1)
-            ans.push_back(i);
-    }
-    return ans;
-}
-
-
-// TC = O(N) + O(N)
-// SC = O(N)
-
-
-// 2nd approach (using math)
-
-vector<int> find_missing_repeating(vector<int> &array) {
-    long long int len = array.size();
-    
-    long long int s = (len*(len+1)) / 2;
-    long long int p = (len*(len+1)*(2*len+1)) / 6;
-    
-    long long int missingElement = 0, repeatingElement = 0;
-    
-    for(int i = 0;i<array.size();i++) {
-        s = s - (long long int)array[i];
-        p = p - (long long int)array[i] * (long long int)array[i];
-    }
-    
-    missingElement = (s + (p/s)) / 2;
-    repeatingElement = missingElement - s;
-    
-    vector<int> ans;
-    
-    ans.push_back(missingElement);
-    ans.push_back(repeatingElement);
-    
-    return ans;
-}
-
-
-// TC = O(N);
-// SC = O(1) not using any extra space
-
-
-// 3rd approach (using XOR)
-
-vector<int> find_missing_repeating(vector<int>& array) {
+// Optimized : XOR
+// TC - O(N)
+// SC - O(1)
+vector<int> find_missing_repeating3(vector<int>& arr) {
     int xor1;
-    
     int set_bit_no;
-    int missingElement = 0, repeatingElement = 0;
-    int n = array.size();
-    
-    xor1 = array[0];
-    
+
+    int i;
+    int x = 0, y = 0;
+    int n = arr.size();
+
+    xor1 = arr[0];
     for(int i = 1;i<n;i++) {
-        xor1 = xor1 ^ array[i];
+        xor1 = xor1 ^ arr[i];
     }
-    
+
     for(int i = 1;i<=n;i++) {
         xor1 = xor1 ^ i;
     }
-    
-    // get the rightmost set bit into set_bit_no
+
     set_bit_no = xor1 & ~(xor1-1);
-    
-    // given element into two bucket
+
     for(int i = 0;i<n;i++) {
-        if(array[i] & set_bit_no)
-            missingElement = missingElement ^ array[i];
-        else
-            repeatingElement = repeatingElement ^ array[i];
+        if(arr[i] & set_bit_no) {
+            x = x^arr[i];
+        }
+        else {
+            y = y^arr[i];
+        }
     }
-    
-    // sepeate (1..N) into two bucket
+
     for(int i = 1;i<=n;i++) {
-        if(i & set_bit_no)
-            missingElement = missingElement ^ i;
-        else
-            repeatingElement = repeatingElement ^ i;
+        if(i & set_bit_no) {
+            x = x^i;
+        }
+        else {
+            y = y^i;
+        }
     }
-    
+
     int x_count = 0;
     for(int i = 0;i<n;i++) {
-        if(array[i] == x)
-            x_count++;
+        if(arr[i] == x) x_count++;
+
     }
-    
-    if(x_count == 0)
-        return {y, x};
-    
-    return {x,y};
+
+    if(x_count == 0) return {y, x};
+
+    return {x, y};
 }
 
+// Using Math
+// TC - O(N)
+// SC - O(1)
+vector<int> find_missing_repeating2(vector<int>& arr) {
+    long long int len = arr.size();
 
-// TC = O(N)
-// SC = O(1)
+    long long int s = (len*(len+1)) / 2;
+    long long int p = (len*(len+1)*(2*len+1)) / 6;
+    int missingNumber = 0, repeatingNumber = 0;
 
+    for(int i = 0;i<len;i++) {
+        s -= (long long int)arr[i];
+        p -= (long long int)arr[i] * (long long int)arr[i];
+    }
 
+    missingNumber = (s + (p/s)) / 2;
+    repeatingNumber = missingNumber - s;
+
+    vector<int> ans;
+    ans.push_back(repeatingNumber);
+    ans.push_back(missingNumber);
+
+    return ans;
+}
+
+// Brute force
+// TC - O(N) + O(N)
+// SC - O(N)
+vector<int> find_missing_repeating(vector<int>& arr) {
+    vector<int> ans;
+    int n = arr.size();
+    int freq[n+1] = {0};
+    for(int i = 0;i<n;i++) {
+        freq[arr[i]]++;
+    }
+
+    for(int i = 1;i<=n;i++) {
+        if(freq[i] > 1) ans.push_back(i);
+
+        if(freq[i] == 0) ans.push_back(i);
+    }
+    return ans;
+}
 int main() {
-	int t;
-	cin >> t;
-	while(t--) {
-		int n;
-		cin >> n;
-		int a[n];
-		for(int i = 0;i<n;i++) {
-			cin >> a[i];
-		}
-
-		auto ans = findTwoElement(arr, n) << endl;
-		cout << ans[0] << " " << ans[1] << endl;
-	}
-	return 0;
+    vector<int> arr = {1,2,3,2,5};
+    vector<int> ans = find_missing_repeating3(arr);
+    for(auto &it : ans) {
+        cout << it << " ";
+    }
 }
